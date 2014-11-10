8b2a051ce111ac7299ec49e6d552d3029b8a6bf2
Troels <Troels.d.hoffmeyer@gmail.com>
2014-11-10 11:44:29 +0100

    Update auto_init.c

--- sys/auto_init/auto_init.c
+++ sys/auto_init/auto_init.c
@@ -78,7 +78,7 @@
 #include "periph/cpuid.h"
 #endif
 
-#define ENABLE_DEBUG (1)
+#define ENABLE_DEBUG (0)
 #if ENABLE_DEBUG
 #define DEBUG_ENABLED
 #endif
@@ -180,6 +180,7 @@ void auto_init_net_if(void)
         }
 
 #endif /* CPUID_ID_LEN && defined(MODULE_HASHES) */
+
         if (net_if_get_pan_id(iface) <= 0) {
             DEBUG("Auto init PAN ID on interface %d to 0x%04x\n", iface, CONF_PAN_ID);
             DEBUG("Change this value at compile time with macro CONF_PAN_ID\n");
